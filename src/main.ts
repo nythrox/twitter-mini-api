402ffda9d3ab5b91321a5a3bbb1eb5975d20c044
Jason Santiago <jasonsantiagobutler@gmail.com>
2020-02-26 17:03:41 -0300

    global validation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+    }),
+  );
+
   await app.listen(8080);
 }
 bootstrap();
